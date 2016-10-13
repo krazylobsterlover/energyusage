4b2f16b050fb907a7ecc9281688d624631e72ac5
aguinane <alexguinane@gmail.com>
2016-10-13 19:36:25 +1000

    Add debug toolbar

--- energy/app.py
+++ energy/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__, static_path='/static', static_folder='static/')
 app.config.from_object('config.Configuration')
+toolbar = DebugToolbarExtension(app)
 db = SQLAlchemy(app)
