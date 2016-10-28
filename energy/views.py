65c1170b0b8eebcb61a4042b73af9444956c88aa
aguinane <alexguinane@gmail.com>
2016-10-29 08:47:55 +1000

    Improve user error handling
    Notify if user doesn't exist.

--- energy/views.py
+++ energy/views.py
@@ -39,7 +39,7 @@ def upload():
 def signup():
     form = UsernamePasswordForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, password=form.password.data)
+        user = User(username=form.username.data.lower(), password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('User created!')
@@ -51,7 +51,10 @@ def signup():
 def signin():
     form = UsernamePasswordForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first_or_404()
+        user = User.query.filter_by(username=form.username.data.lower()).first()
+        if user is None:
+            flash('No user with that name found: ' + str(form.username.data.lower()))
+            return redirect(url_for('signin'))
         if user.is_correct_password(form.password.data):
             login_user(user)
             return redirect(url_for('index'))
