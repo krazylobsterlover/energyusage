5542860c25aa5d605f29092cedccb1d388f8fa35
aguinane <alexguinane@gmail.com>
2016-11-11 19:23:43 +1000

    Keep login context
    Re-direct to the page the user wanted after login

--- energy/views.py
+++ energy/views.py
@@ -84,7 +84,7 @@ def signin():
             return redirect(url_for('signin'))
         if user.is_correct_password(form.password.data):
             login_user(user)
-            return redirect(url_for('index'))
+            return redirect(request.args.get('next') or url_for('index'))
         else:
             return redirect(url_for('signin'))
     return render_template('signin.html', form=form)
