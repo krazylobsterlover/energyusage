c9ba9e766bccdff262d9b4d2c4c57081d21bf190
aguinane <alexguinane@gmail.com>
2016-11-13 17:49:51 +1000

    Create nescessary folders
    When running check required folders that are ignored by git are created. Add logging to console back again.

--- run.py
+++ run.py
@@ -1,8 +1,26 @@
+import logging
+import logging.config
+import sys
+import yaml
 from energy import app
 from energy.views import *
 
 if __name__ == '__main__':
 
-    import logging
-    logging.basicConfig(filename='logs/error.log',level=logging.WARNING)
-    app.run(host= '0.0.0.0', debug=True)
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    with open('logging.yaml', 'rt') as f:
+        config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+    app.run(host='0.0.0.0', debug=True)
+
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    if not os.path.isfile(app.config['DATABASE']):
+        from energy import db
+        db.create_all()
